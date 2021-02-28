e887daaa1c66d3024e2ebee3a283d939a2b53b1f
Nes <epmjj4@hotmail.com>
2021-02-28 11:55:44 -0500

    updated varialbe and created runApp function

--- app.js
+++ app.js
@@ -10,7 +10,16 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const empArr = [];
+const idARR = [];
 
+const runApp = () = {
+
+
+
+
+    
+}
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
